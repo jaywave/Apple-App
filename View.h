2969847e3a8011a0be02aa1ec834d290d4eddf2c
JAYAVELU <jayaveluraj@users.noreply.github.com>
2014-09-01 16:46:46 +0530

    Initial Commit

---
+++ View.h
@@ -0,0 +1,18 @@
+//
+//  View.h
+//  Sample Application
+//
+//  Created by Jayavelu on 9/1/14.
+//  Copyright (c) 2014 Jayavelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIViewController
+{
+    IBOutlet UILabel *Hello;
+    
+}
+-(void)But:(id)sender;
+
+@end
---
+++ View.m
@@ -0,0 +1,42 @@
+//
+//  View.m
+//  Sample Application
+//
+//  Created by Jayavelu on 9/1/14.
+//  Copyright (c) 2014 Jayavelu. All rights reserved.
+//
+
+#import "View.h"
+
+@interface View ()
+
+@end
+
+@implementation View
+-(void)But:(id)sender
+{
+    Hello.text=[NSString stringWithFormat:@"HELLO WORLD"];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
